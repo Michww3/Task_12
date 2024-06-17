61efe3885531afebec517b6f1ef4a49c626685e7
User <User@W11PC>
2024-06-17 15:29:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main (string[] args)
+    {
+        AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+        Thread thread1 = new Thread(ThreadWork1);
+        Thread thread2 = new Thread(ThreadWork2);
+
+        thread1.Start();
+        thread2.Start();
+
+        autoResetEvent.WaitOne();
+
+        Console.WriteLine("Unreachable text");
+    }
+
+    static void ThreadWork1()   
+    {
+        Console.WriteLine("Work1");
+    }
+    static void ThreadWork2()
+    {
+        Console.WriteLine("Work2");
+    }
+}
