77f4b73d86a76d8c1ab15272bac1c4126b22dd12
Michww3 <michww3@gmail.com>
2024-06-27 21:46:17 +0300

    new

--- Program.cs
+++ Program.cs
@@ -2,23 +2,25 @@
 {
     static void Main (string[] args)
     {
-        AutoResetEvent autoResetEvent = new AutoResetEvent(false);
-        Thread thread1 = new Thread(ThreadWork1);
-        Thread thread2 = new Thread(ThreadWork2);
+        var autoResetEvent = new AutoResetEvent(false);
+        //Thread thread1 = new Thread(ThreadWork1);
+        //Thread thread2 = new Thread(ThreadWork2);
 
-        thread1.Start();
-        thread2.Start();
+        //thread1.Start();
+        //thread2.Start();
+        ThreadPool.QueueUserWorkItem(ThreadWork1);
+        ThreadPool.QueueUserWorkItem(ThreadWork2);
 
         autoResetEvent.WaitOne();
 
         Console.WriteLine("Unreachable text");
     }
 
-    static void ThreadWork1()   
+    static void ThreadWork1(object state)   
     {
         Console.WriteLine("Work1");
     }
-    static void ThreadWork2()
+    static void ThreadWork2(object state)
     {
         Console.WriteLine("Work2");
     }
